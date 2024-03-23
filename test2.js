1e19294518070e3d30dd2a61a88f5f20ca6152f5
akash <akashmurali24@gmail.com>
2024-03-23 09:46:56 +0530

    new message

--- test2.js
+++ test2.js
@@ -4,3 +4,9 @@ const handleDelete = (userid: string) => {
   setUsers(updatedUsers);
   localStorage.setItem("userList", JSON.stringify({ results: updatedUsers }));
 };
+const handleDelete = (userid: string) => {
+  const updatedUsers = users.filter((user) => user.login.uuid !== userid);
+  setTotalUsers(updatedUsers.length);
+  setUsers(updatedUsers);
+  localStorage.setItem("userList", JSON.stringify({ results: updatedUsers }));
+};
