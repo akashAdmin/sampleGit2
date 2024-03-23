643dc126888079f90bff45b39949fdc2d434ce6e
akash <akashmurali24@gmail.com>
2024-03-23 11:09:46 +0530

    added

--- test2.js
+++ test2.js
@@ -4,10 +4,14 @@ const handleDelete = (userid: string) => {
   setUsers(updatedUsers);
   localStorage.setItem("userList", JSON.stringify({ results: updatedUsers }));
 };
-
-const handleAdd = (userid: string) => {
-  const userUpdate = users.map((user) => {
-    user.login.uuid !== user;
-  });
-  setTotalUsers(userUpdate);
-};
+const handleDelete = (userid: string) => {
+  const updatedUsers = users.filter((user) => user.login.uuid !== userid);
+  setTotalUsers(updatedUsers.length);
+  setUsers(updatedUsers);
+  localStorage.setItem("userList", JSON.stringify({ results: updatedUsers }));
+  const handleAdd = (userid: string) => {
+    const userUpdate = users.map((user) => {
+      user.login.uuid !== user;
+    });
+    setTotalUsers(userUpdate);
+  };
